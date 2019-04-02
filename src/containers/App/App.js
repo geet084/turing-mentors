f93ac336039c2c7d4d00c60b0f9164bbff02334c
Travis Gee <geet084@gmail.com>
2019-04-02 17:23:05 -0600

    Moves app into it's own folder

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Nav from '../../components/Nav/Nav';
+import HeroImage from '../../components/HeroImage/HeroImage';
+import Display from '../Display/Display';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <h1>Turing Mentors</h1>
+        <Nav />
+        <HeroImage />
+        <Display />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App';
+import App from './containers/App/App';
 import './main.scss';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
