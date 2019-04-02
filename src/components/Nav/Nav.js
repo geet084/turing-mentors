622b2a1d865c5e97db18e8dd61311e526a8d1e81
Travis Gee <geet084@gmail.com>
2019-04-02 17:56:36 -0600

    Adds nav component

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import logo from '../../images/t-logo.png'
 
 export const Nav = () => {
-  return (<div>NAV</div>)
+  return (
+    <nav className="nav">
+      <div>
+        <img className="logo" src={logo} alt="turing logo" />
+      </div>
+      <ul className="links">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact Us</li>
+        <li>Contribute</li>
+      </ul>
+    </nav>
+  )
 }
 
 export default Nav;
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -8,7 +8,6 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>Turing Mentors</h1>
         <Nav />
         <HeroImage />
         <Display />
