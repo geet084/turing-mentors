b9b7de98c985dbd4112fbfd4a22674078bcee76f
Travis Gee <geet084@gmail.com>
2019-04-03 07:48:54 -0600

    Adds mentee component

---
+++ src/containers/Mentee/Mentee.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Mentee extends Component {
+  render() {
+    return (
+      <div>mentee form</div>
+    )
+  }
+}
+
+export default Mentee;
