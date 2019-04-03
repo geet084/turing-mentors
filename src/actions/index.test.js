9de5bc553416205494dad65677742c4d3f83e17e
Travis Gee <geet084@gmail.com>
2019-04-02 19:04:04 -0600

    Adds hasErrored action test

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,11 +1,19 @@
-import * as actions from './index'
+import * as actions from './index';
 
 describe('actions', () => {
   it('should return the isLoading action with a type and bool', () => {
-    const bool = true
-    const expected = { type: 'IS_LOADING', isLoading: true }
+    const bool = true;
+    const expected = { type: 'IS_LOADING', isLoading: true };
 
-    const result = actions.isLoading(bool)
-    expect(result).toEqual(expected)
+    const result = actions.isLoading(bool);
+    expect(result).toEqual(expected);
+  })
+
+  it('should return the hasErrored action with a type and a message', () => {
+    const message = 'Error';
+    const expected = { type: 'HAS_ERRORED', message: 'Error' };
+
+    const result = actions.hasErrored(message);
+    expect(result).toEqual(expected);
   })
 })
