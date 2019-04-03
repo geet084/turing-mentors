96509402805468d1ebe4e4f7cd2607ac2204cafa
Travis Gee <geet084@gmail.com>
2019-04-03 07:49:00 -0600

    Adds mentor component

---
+++ src/containers/Mentor/Mentor.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Mentor extends Component {
+  render() {
+    return (
+      <div>mentor form</div>
+    )
+  }
+}
+
+export default Mentor;
