f0295f6633000ed8be6dfe141aac9ca91b90a2df
Travis Gee <geet084@gmail.com>
2019-04-03 20:43:16 -0600

    Adds a form for the mentee component

--- src/containers/Mentee/Mentee.js
+++ src/containers/Mentee/Mentee.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
+import UserInfo from '../../components/UserInfo/UserInfo';
+import UserBio from '../../components/UserBio/UserBio';
+import UserSchedule from '../../components/UserSchedule/UserSchedule';
 
 export class Mentee extends Component {
+  state = {
+    userInfo: {},
+    userBio: {},
+    userSchedule: {},
+    currentSection: 'userInfo',
+    // currentSection: 'userBio',
+    // currentSection: 'userSchedule',
+  }
+
+  updateUserInfo = (info) => {
+    const updatedSection = this.state.currentSection;
+    const currentSection = info.pop();
+
+    this.setState({ [updatedSection]: info.pop(), currentSection })
+  }
+
+  submitForm = (e) => {
+    e.preventDefault();
+  }
+
   render() {
+    console.log('aa', this.state)
+    const { currentSection } = this.state;
+
     return (
-      <div>MENTEE FORM HERE</div>
+      <div>
+        {currentSection === 'userInfo' && <UserInfo updateUserInfo={this.updateUserInfo} />}
+        {currentSection === 'userBio' && <UserBio updateUserInfo={this.updateUserInfo} />}
+        {currentSection === 'userSchedule' && <UserSchedule updateUserInfo={this.updateUserInfo} />}
+        {this.state.currentSection === 'complete' && <div><h1>ALL DONE</h1></div>}
+      </div>
     )
   }
 }
