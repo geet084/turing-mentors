e192024892f879600c85a6b2d877e4c774e4b350
Travis Gee <geet084@gmail.com>
2019-04-05 17:07:55 -0600

    Adds test for formChoice component

---
+++ src/components/FormChoice/FormChoice.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import FormChoice from './FormChoice';
+
+describe('FormChoice', () => {
+  let wrapper;
+  it('should have match the correct snapshot', () => {
+    wrapper = shallow(<FormChoice />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
