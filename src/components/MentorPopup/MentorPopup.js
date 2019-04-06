ca6f0f61737cd9d4dffd2e911da9f7357ef9a461
Travis Gee <geet084@gmail.com>
2019-04-06 13:59:05 -0600

    Adds mentorPopup component for more details on each mentor

---
+++ src/components/MentorPopup/MentorPopup.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export class MentorPopup extends Component {
+  render() {
+    return (
+      <div>
+        mentor popup page
+        <p>{this.props.name}</p>
+        <p>{this.props.location}</p>
+        <p>{this.props.cohort}</p>
+        <p>{this.props.background}</p>
+        <p>{this.props.tech_skills[0]}</p>
+        <Link to='/mentors'>GO BACK</Link>
+      </div>
+    )
+  }
+}
+
+export default MentorPopup;
