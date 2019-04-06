a9ea4dc1e40abc8a124190706afaf0373530fe90
Travis Gee <geet084@gmail.com>
2019-04-06 09:39:14 -0600

    Adds new components for about, contact and contribute

--- src/containers/Display/Display.js
+++ src/containers/Display/Display.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import About from '../../components/About/About';
+import Contact from '../../components/Contact/Contact';
+import Contribute from '../../components/Contribute/Contribute';
 import FormChoice from '../../components/FormChoice/FormChoice';
 import Mentor from '../Mentor/Mentor';
 import Mentee from '../Mentee/Mentee';
@@ -11,6 +14,9 @@ export class Display extends Component {
       <main className="display">
         <Switch>
           <Route exact path='/' component={FormChoice} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/contact' component={Contact} />
+          <Route exact path='/contribute' component={Contribute} />
           <Route exact path='/mentor' component={Mentor} />
           <Route exact path='/mentee' component={Mentee} />
           <Route component={NotFound} />
