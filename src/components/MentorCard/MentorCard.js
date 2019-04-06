3e9924a44bd4f3e71e2d9eee8d1b803fec8060e4
Travis Gee <geet084@gmail.com>
2019-04-06 12:16:51 -0600

    Adds mentorCard component and implements a basic display

---
+++ src/components/MentorCard/MentorCard.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export class MentorCard extends Component {
+  render() {
+    const { name, location, tech_skills } = this.props;
+    return (
+      <div className="mentors-card">
+        <p>{name}</p>
+        <div className="loc-lang">
+          <span className="loc">
+            {location}
+          </span>
+          <span className="lang">
+            {tech_skills[0]}
+          </span>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default MentorCard;
