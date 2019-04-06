0436d252d0ef298e4bb83b0fbb690d7c15964fef
Travis Gee <geet084@gmail.com>
2019-04-06 13:58:44 -0600

    Adds link functionality to click into a specific mentor

--- src/components/MentorCard/MentorCard.js
+++ src/components/MentorCard/MentorCard.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export class MentorCard extends Component {
   render() {
-    const { name, location, tech_skills } = this.props;
+    const { id, name, location, tech_skills } = this.props;
     return (
-      <div className="mentors-card">
-        <p>{name}</p>
-        <div className="loc-lang">
-          <span className="loc">
-            {location}
-          </span>
-          {
-            tech_skills &&
-            <span className="lang">
-              {tech_skills[0]}
+      <Link to={`/mentors/${id}`}>
+        <div className="mentors-card">
+          <p>{name}</p>
+          <div className="loc-lang">
+            <span className="loc">
+              {location}
             </span>
-          }
+            {
+              tech_skills &&
+              <span className="lang">
+                {tech_skills[0]}
+              </span>
+            }
+          </div>
         </div>
-      </div>
+      </Link>
     )
   }
 }
