1bc04e567df7dc4c86e74a36b7ad6dabcc4b52bd
Travis Gee <geet084@gmail.com>
2019-04-06 09:37:37 -0600

    Adds about component

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class About extends Component {
+  render() {
+    return (
+      <div>
+        <h2>About page</h2>
+        <p>Turing mentors is about...</p>
+      </div>
+    )
+  }
+}
+
+export default About;
