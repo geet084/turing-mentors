4f056a690d92992946351a8fc90d47963d0dcf24
Travis Gee <geet084@gmail.com>
2019-04-06 09:38:02 -0600

    Adds contact component

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Contact page</h2>
+        <p>Turing contact info...</p>
+      </div>
+    )
+  }
+}
+
+export default Contact;
