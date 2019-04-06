12ae2255398ff3035bbbb96812956381b492f504
Travis Gee <geet084@gmail.com>
2019-04-05 18:15:52 -0600

    Adds initial MSTP test for Mentee component

--- src/containers/Mentee/Mentee.test.js
+++ src/containers/Mentee/Mentee.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Mentee from './Mentee';
+import { mapStateToProps } from './Mentee';
 
 describe('Mentee', () => {
   let wrapper;
@@ -8,5 +9,17 @@ describe('Mentee', () => {
   it('should have match the correct snapshot', () => {
     wrapper = shallow(<Mentee />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('mapStateToProps', () => {
+    it('should return an object with all form details', () => {
+      const mockState = { firstName: '', lastName: '' };
+      
+      const expected = {};
+      
+      const mappedProps = mapStateToProps(mockState);
+      
+      expect(mappedProps).toEqual(expected);
+    });
   });
 });
