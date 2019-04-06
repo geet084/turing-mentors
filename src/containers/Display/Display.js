623a5129805b28f2b3b117406b845b12a1f29488
Travis Gee <geet084@gmail.com>
2019-04-06 13:59:32 -0600

    Adds routing for a specific mentor when selected

--- src/containers/Display/Display.js
+++ src/containers/Display/Display.js
@@ -6,8 +6,10 @@ import Contribute from '../../components/Contribute/Contribute';
 import FormChoice from '../../components/FormChoice/FormChoice';
 import Mentor from '../Mentor/Mentor';
 import Mentors from '../Mentors/Mentors';
+import MentorPopup from '../../components/MentorPopup/MentorPopup';
 import Mentee from '../Mentee/Mentee';
 import NotFound from '../../components/NotFound/NotFound';
+import { connect } from 'react-redux';
 
 export class Display extends Component {
   render() {
@@ -19,6 +21,13 @@ export class Display extends Component {
           <Route exact path='/contact' component={Contact} />
           <Route exact path='/contribute' component={Contribute} />
           <Route exact path='/mentors' component={Mentors} />
+          <Route path='/mentors/:id' render={({ match }) => {
+            const { id } = match.params;
+            const mentor = this.props.mentors.find(mentor => mentor.id === id);
+
+            if (mentor) return <MentorPopup id={mentor.id} {...mentor.attributes} />
+            else return <NotFound />
+          }} />
           <Route exact path='/mentor' component={Mentor} />
           <Route exact path='/mentee' component={Mentee} />
           <Route component={NotFound} />
@@ -28,4 +37,8 @@ export class Display extends Component {
   }
 }
 
-export default Display;
+export const mapStateToProps = (state) => ({
+  mentors: state.mentors,
+})
+
+export default connect(mapStateToProps)(Display);
