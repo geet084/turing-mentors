60bf6cefd46e5ef06c2ee535f6a85e2e76443eb7
Travis Gee <geet084@gmail.com>
2019-04-06 12:15:51 -0600

    Adds masonry for display and fetch functionality to mentors

--- src/containers/Mentors/Mentors.js
+++ src/containers/Mentors/Mentors.js
@@ -1,17 +1,51 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { getMentors } from '../../thunks/getMentors';
+import Masonry from 'react-masonry-component';
+import MentorCard from '../../components/MentorCard/MentorCard';
 
 export class Mentors extends Component {
 
   componentDidMount = () => {
-
+    const corsPrefix = 'https://cors-anywhere.herokuapp.com/';
+    const root = 'https://sheltered-beach-23653.herokuapp.com';
+    const path = '/api/v1/mentors?location=all';
+    // const url = process.env.REACT_APP_BACKEND_URL + path
+    const url = corsPrefix + root + path
+    this.props.getMentors(url);
   }
 
   render() {
+    const { mentors } = this.props;
+    const mentorList = mentors.map((mentor, i) => {
+      return <MentorCard key={i} {...mentor.attributes} />
+    });
+
+    const breakpointColumnsObj = {
+      default: 4,
+      1100: 3,
+      700: 2,
+      500: 1
+    };
+
     return (
-      <div>display mentors here</div>
+      <div className="mentors-container">
+        <Masonry breakpointCols={breakpointColumnsObj}
+          className="mentors-grid"
+          columnClassName="mentors-grid_column">
+          {mentorList}
+        </Masonry>
+      </div>
     )
   }
 }
 
-export default Mentors;
+export const mapStateToProps = (state) => ({
+  mentors: state.mentors,
+})
+
+export const mapDispatchToProps = (dispatch) => ({
+  getMentors: (url) => dispatch(getMentors(url)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Mentors);
