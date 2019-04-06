7f6c274e82ec7905b448124353769418e2cf8869
Travis Gee <geet084@gmail.com>
2019-04-06 09:38:24 -0600

    Adds contribute component

---
+++ src/components/Contribute/Contribute.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class Contribute extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Contribute page</h2>
+        <p>Turing mentors contribution...</p>
+      </div>
+    )
+  }
+}
+
+export default Contribute;
