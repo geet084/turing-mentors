5f61afe80f4a72bda49085a184e9a4bdb7d1b48c
Travis Gee <geet084@gmail.com>
2019-04-05 18:04:35 -0600

    Adds initial test for userBio component tests

---
+++ src/components/UserBio/UserBio.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import UserBio from './UserBio';
+
+describe('UserBio', () => {
+  let wrapper;
+  const mockProps = {
+    updateUserInfo: jest.fn()
+  };
+
+  beforeEach(() => {
+    wrapper = shallow(<UserBio {...mockProps} />);
+  });
+
+  it('should match the correct snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it.skip('should register a change when the input is typed in', () => {
+    const mockEvent = { target: { value: 'new info', name: 'slack' } };
+    const expected = {
+      slack: 'new info',
+      email: '',
+      phone: ''
+    };
+
+    wrapper.find('.input-effect').simulate('change', mockEvent);
+    expect(wrapper.state()).toEqual(expected);
+  });
+});
