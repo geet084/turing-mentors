39c364d719acefa24ec62f53cc51800a833f9626
Travis Gee <geet084@gmail.com>
2019-04-06 12:14:32 -0600

    Adds getMentors thunk for fetching all mentors

---
+++ src/thunks/getMentors.js
@@ -0,0 +1,19 @@
+import { isLoading, hasErrored, getMentorsSuccess } from '../actions';
+
+export const getMentors = (url) => {
+  return async (dispatch) => {
+    try {
+      dispatch(isLoading(false));
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      dispatch(isLoading(false));
+      const results = await response.json();
+      dispatch(getMentorsSuccess(results.data));
+    } catch (error) {
+      dispatch(hasErrored(error.message));
+      dispatch(isLoading(false));
+    }
+  }
+}
