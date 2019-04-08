fb38af085df840c62a386a1f5f01a2dd4bd254db
Travis Gee <geet084@gmail.com>
2019-04-08 08:37:22 -0600

    Adds initial nonTechSkills functionality

--- src/components/UserNonTechSkills/UserNonTechSkills.js
+++ src/components/UserNonTechSkills/UserNonTechSkills.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
+import { CreateCheckbox } from '../CreateCheckbox/CreateCheckbox';
 
 export class UserNonTechSkills extends Component {
+  state = {
+    nonTechSkills: {
+      StressManagement: false,
+      PublicSpeaking: false,
+      Resumes: false,
+      Technical_Interview: false,
+      Parenting: false,
+      Wellness: false,
+    }
+  }
+
   submitForm = (e) => {
     e.preventDefault();
     this.props.updateUserInfo([this.state, 'complete']);
@@ -11,12 +23,26 @@ export class UserNonTechSkills extends Component {
     this.props.updateUserInfo([this.state, 'userTechSkills']);
   }
 
+  checkBoxes = ({ target }) => {
+    let nonTechSkills = { ...this.state.nonTechSkills };
+    nonTechSkills[target.name] = !nonTechSkills[target.name]
+
+    this.setState({ nonTechSkills })
+  }
+
   render() {
     const mentor = this.props.user === 'Mentor';
+    const { nonTechSkills } = this.state;
+    const checkBoxes = Object.keys(nonTechSkills).map((skill, index) => {
+      return <CreateCheckbox key={skill} field={skill} name={skill} value={index} checkBoxes={this.checkBoxes} />
+    })
 
     return (
       <div className="form-page">
-        <h2>NON tech skills checkboxes here</h2>
+        <p className="pages">Mentor NON Tech skills</p>
+        <form>
+          {checkBoxes}
+        </form>
         <button className="next-btn" onClick={this.goBack}>Back</button>
         <button className="next-btn" onClick={this.submitForm}>Submit</button>
         <span className="pages">6 of {mentor ? '6' : '4'}</span>
