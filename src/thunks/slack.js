46251f65351dfaff67a017a964672a6d3855e46c
Travis Gee <geet084@gmail.com>
2019-04-08 13:10:21 -0600

    Adds slack thunk

---
+++ src/thunks/slack.js
@@ -0,0 +1,24 @@
+import { isLoading, hasErrored, slackSuccess } from '../actions';
+
+export const slack = (url, text) => {
+  return async (dispatch) => {
+    try {
+      dispatch(isLoading(true));
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        Authorization: 'Bearer xoxp- 589107782211 - 589208192226 - 600625502240 - 86d5dcab2922f8b0504a15a263e35aa4',
+        body: JSON.stringify({ text })
+      })
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      dispatch(isLoading(false));
+      const results = await response.json();
+      dispatch(slackSuccess(results.data));
+    } catch (error) {
+      dispatch(hasErrored(error.message));
+      dispatch(isLoading(false));
+    }
+  }
+}
