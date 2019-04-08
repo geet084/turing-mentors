01ac35c38093a88d6cbb57c4c6952cccb4915dc4
Travis Gee <geet084@gmail.com>
2019-04-08 15:19:02 -0600

    Adds tests for Mentors component

--- src/containers/Mentors/Mentors.test.js
+++ src/containers/Mentors/Mentors.test.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Mentors from './Mentors';
+import { Mentors, mapStateToProps, mapDispatchToProps } from './Mentors';
+import { getMentors } from '../../actions';
 
 describe('Mentors', () => {
   let wrapper;
+  const mockProps = {
+    getMentors: jest.fn(),
+    mentors: [{ id: 1, first_name: 'first' }, { id: 2, first_name: 'second' }]
+  }
 
-  it('should have match the correct snapshot', () => {
-    wrapper = shallow(<Mentors />);
-    expect(wrapper).toMatchSnapshot();
+  beforeEach(() => {
+    wrapper = shallow(<Mentors {...mockProps} />);
+  });
+
+  describe('initial', () => {
+    it('should have match the correct snapshot', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+
+  describe('mapStateToProps', () => {
+    it('should return an array of mentors', () => {
+      const mockState = {
+        mentors: [],
+        stuff: {},
+        things: '',
+      };
+
+      const expected = {
+        mentors: []
+      };
+
+      const mappedProps = mapStateToProps(mockState);
+
+      expect(mappedProps).toEqual(expected);
+    });
+  });
+
+  describe('mapDispatchToProps', () => {
+    it('should call getMentors dispatch', () => {
+      const mockDispatch = jest.fn();
+      const mockURL = 'http://localhost:3010';
+      //TODO: adjust to pass a more robust test
+      // const actionToDispatch = getMentors(mockURL);
+
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.getMentors(mockURL);
+
+      // expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+      expect(mockDispatch).toHaveBeenCalled();
+    });
   });
 });
