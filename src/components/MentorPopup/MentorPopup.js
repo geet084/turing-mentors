7d560cde4a1f706c1271a79ac003c8a48c1f2159
Travis Gee <geet084@gmail.com>
2019-04-08 13:09:36 -0600

    Adds slack message text area state management

--- src/components/MentorPopup/MentorPopup.js
+++ src/components/MentorPopup/MentorPopup.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link  } from 'react-router-dom';
 
 export class MentorPopup extends Component {
+  state = {
+    text: ''
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ text: target.value })
+  }
+
+  handleSubmit = () => {
+    const message = `To user: ${this.props.slack ? this.props.slack : 'anonymous'} -- message: ${this.state.text}`
+    this.setState({ text: '' })
+    this.props.sendMessage(message)
+  }
+
   render() {
     return (
       <div>
@@ -11,6 +25,10 @@ export class MentorPopup extends Component {
         <p>{this.props.cohort}</p>
         <p>{this.props.background}</p>
         <p>{this.props.tech_skills && this.props.tech_skills[0]}</p>
+        <div>
+          <textarea className="bg-info" name="text" placeholder="send slack message" value={this.state.text} onChange={this.handleChange}></textarea>
+        </div>
+        <button onClick={this.handleSubmit} >Submit</button>
         <Link to='/mentors'>GO BACK</Link>
       </div>
     )
