ec910b2d4e20f94b00986825aaa81f18f6e0f5aa
Travis Gee <geet084@gmail.com>
2019-04-09 16:50:29 -0600

    Adds info to the contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,9 +3,13 @@ import React, { Component } from 'react';
 export class Contact extends Component {
   render() {
     return (
-      <div>
-        <h2>Contact page</h2>
-        <p>Turing contact info...</p>
+      <div className="link-content pages">
+        <h2>Turing School of Software and Design</h2>
+        <p>1331 17th Street</p>
+        <p>Suite LL100</p>
+        <p>Denver, CO 80202</p>
+        <p>Phone: 303.731.3117</p>
+        <p>Fax: 303.374.7360</p>
       </div>
     )
   }
