c3f110cd64ddb668988b96a8cacea6aa8153b25f
Travis Gee <geet084@gmail.com>
2019-04-09 16:50:51 -0600

    Adds info and styling to the About component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,9 +3,16 @@ import React, { Component } from 'react';
 export class About extends Component {
   render() {
     return (
-      <div>
-        <h2>About page</h2>
-        <p>Turing mentors is about...</p>
+      <div className="link-content pages">
+        <p>
+          Turing's mission is to unlock human potential by training a diverse and inclusive student body to succeed in high-fulfillment technical careers. We are a community committed to challenging ourselves and each other to build the knowledge, skills, and understanding needed for long-term success.
+          </p>
+        <p>
+          In addition to that committment is the willingness to help others who are walking in your footsteps, struggling in the same ways you may have struggled.
+          </p>
+        <h5>
+          Turing Mentors offers a way to connect those on both sides of the Turing program to the betterment of all.
+          </h5>
       </div>
     )
   }
