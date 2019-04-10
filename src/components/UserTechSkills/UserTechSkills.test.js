83c5c35c4d6fb4cda79c4e8dc2f682632c6132e4
Travis Gee <geet084@gmail.com>
2019-04-10 08:46:44 -0600

    Adds tests for userTechSkills component

--- src/components/UserTechSkills/UserTechSkills.test.js
+++ src/components/UserTechSkills/UserTechSkills.test.js
@@ -7,12 +7,39 @@ describe('UserTechSkills', () => {
   const mockProps = {
     updateUserInfo: jest.fn()
   };
+  const mockState = {
+    tech_skills: []
+  };
 
   beforeEach(() => {
     wrapper = shallow(<UserTechSkills {...mockProps} />);
   });
 
-  it('should match the correct snapshot', () => {
-    expect(wrapper).toMatchSnapshot();
+  describe('initial snapshot', () => {
+    it('should match the correct snapshot', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+
+  describe('submitForm', () => {
+    it('should handle the form submission', () => {
+      const mockEvent = { preventDefault: () => { } };
+      const expected = [mockState, 'userNonTechSkills'];
+
+      wrapper.instance().submitForm(mockEvent);
+
+      expect(mockProps.updateUserInfo).toHaveBeenCalledWith(expected);
+    });
+  });
+
+  describe('goBack', () => {
+    it('should handle going back a page', () => {
+      const mockEvent = { preventDefault: () => { } };
+      const expected = [mockState, 'userSchedule'];
+
+      wrapper.instance().goBack(mockEvent);
+
+      expect(mockProps.updateUserInfo).toHaveBeenCalledWith(expected);
+    });
   });
 });
