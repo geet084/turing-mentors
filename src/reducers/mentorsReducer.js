e8a5bbca4721391b67125eafcbf342a7edb73a90
Travis Gee <geet084@gmail.com>
2019-04-09 19:43:06 -0600

    Adds postMentorSuccess action

--- src/reducers/mentorsReducer.js
+++ src/reducers/mentorsReducer.js
@@ -2,6 +2,8 @@ export const mentorsReducer = (state = [], action) => {
   switch (action.type) {
     case 'GET_MENTORS_SUCCESS':
       return action.mentors;
+    case 'POST_MENTOR_SUCCESS':
+      return [...state, action.mentor];
     default:
       return state;
   }
