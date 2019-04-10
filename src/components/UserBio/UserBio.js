bb7ea4acf7e0325f9c12baff290df1d1843cbb95
Travis Gee <geet084@gmail.com>
2019-04-09 19:43:53 -0600

    Adds location field to form

--- src/components/UserBio/UserBio.js
+++ src/components/UserBio/UserBio.js
@@ -6,6 +6,7 @@ export class UserBio extends Component {
     slack: '',
     email: '',
     phone: '',
+    location: '',
   }
 
   submitForm = (e) => {
@@ -23,16 +24,20 @@ export class UserBio extends Component {
   }
 
   render() {
-    const { slack, email, phone } = this.state;
+    const { slack, email, phone, location } = this.state;
     const mentor = this.props.user === 'Mentor';
 
     return (
       <div>
         <form onSubmit={this.submitForm} autoComplete='off'>
           <span className="pages">{this.props.user} Contact info:</span>
-            <CreateInput field="slack" text="Slack Handle" value={slack} handleChange={this.handleChange} max="28" />
-            <CreateInput field="email" text="Email Address" value={email} handleChange={this.handleChange} max="28" />
-            <CreateInput field="phone" text="Phone Number" value={phone} handleChange={this.handleChange} max="12" />
+          <CreateInput field="slack" text="Slack Handle" value={slack} handleChange={this.handleChange} max="28" />
+          <CreateInput field="email" text="Email Address" value={email} handleChange={this.handleChange} max="28" />
+          <CreateInput field="phone" text="Phone Number" value={phone} handleChange={this.handleChange} max="12" />
+          {
+            mentor &&
+            <CreateInput field="location" text="Location" value={location} handleChange={this.handleChange} max="28" />
+          }
         </form>
         <button className="next-btn" onClick={this.goBack}>Back</button>
         <button className="next-btn" onClick={this.submitForm}>Next</button>
