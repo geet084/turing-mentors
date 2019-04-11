2d47ab0d2bd3c868b54b0e3463c5454813d45ce5
Travis Gee <geet084@gmail.com>
2019-04-11 07:23:45 -0600

    Adds mentorControls tests

--- src/containers/MentorControls/MentorControls.test.js
+++ src/containers/MentorControls/MentorControls.test.js
@@ -17,8 +17,38 @@ describe('MentorControls', () => {
   });
 
   describe('initial', () => {
-    it('should have match the correct snapshot', () => {
+    it('should have match the correct default snapshot', () => {
       expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should have match the correct snapshot for javascript', () => {
+      wrapper.setProps({ javascript: true });
+      expect(wrapper).toMatchSnapshot();
+    });
+   
+    it('should have match the correct snapshot for ruby', () => {
+      wrapper.setProps({ ruby: true });
+      expect(wrapper).toMatchSnapshot();
+    });
+   
+    it('should have match the correct snapshot for denver', () => {
+      wrapper.setProps({ denver: true });
+      expect(wrapper).toMatchSnapshot();
+    });
+   
+    it('should have match the correct snapshot for remote', () => {
+      wrapper.setProps({ remote: true });
+      expect(wrapper).toMatchSnapshot();
+    });
+  });
+
+  describe('checkBoxes', () => {
+    it('should update the selected input in state', () => {
+      const mockEvent = { preventDefault: () => { } };
+
+      wrapper.instance().checkBoxes(mockEvent);
+
+      expect(mockProps.handleChange).toHaveBeenCalledWith(mockEvent);
     });
   });
 });
