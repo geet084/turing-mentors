01b19b25dc696feec3584c8ea53b95edd2e6ffea
Travis Gee <geet084@gmail.com>
2019-04-24 13:12:21 -0600

    Adds the ability to track previously entered info for UserInfo component

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -3,13 +3,17 @@ import { CreateInput } from '../CreateInput/CreateInput';
 import { CreateCheckbox } from '../CreateCheckbox/CreateCheckbox';
 
 export class UserInfo extends Component {
-  state = {
-    first_name: '',
-    last_name: '',
-    identities: [],
-    cohort: 0,
-    program: '',
-    current_job: '',
+  constructor({ first_name, last_name, identities, cohort, program, current_job }) {
+    super({ first_name, last_name, identities, cohort, program, current_job })
+
+    this.state = {
+      first_name: first_name || '',
+      last_name: last_name || '',
+      identities: identities || [],
+      cohort: cohort || 0,
+      program: program || '',
+      current_job: current_job || '',
+    }
   }
 
   submitForm = (e) => {
@@ -41,12 +45,12 @@ export class UserInfo extends Component {
   }
 
   render() {
-    const { first_name, last_name, cohort, current_job } = this.state;
+    const { first_name, last_name, identities, cohort, program, current_job } = this.state;
     const mentor = this.props.user === 'Mentor';
-    const identities = ['Male', 'Female', 'Non-Binary'].map((identity, i) => (
-      <CreateCheckbox key={i} field="identities" name={identity} value={i + 1} checkBoxes={this.updateIdentity} />
+    const identityBoxes = ['Male', 'Female', 'Non-Binary'].map((identity, i) => (
+      <CreateCheckbox key={i} field="identities" name={identity} value={i + 1} checkBoxes={this.updateIdentity} checked={identities.includes(i + 1) ? true : false} />
     ))
-    
+
     return (
       <div>
         <form onSubmit={this.submitForm} autoComplete='off'>
@@ -54,12 +58,12 @@ export class UserInfo extends Component {
           <CreateInput field="first_name" text="First Name" value={first_name} handleChange={this.handleChange} max="28" />
           <CreateInput field="last_name" text="Last Name" value={last_name} handleChange={this.handleChange} max="28" />
           <div className="check-box">
-            {identities}
+            {identityBoxes}
           </div>
           <div>
-            <CreateInput field="cohort" text="Cohort (ex: 1406)" value={cohort} handleChange={this.updateCohort} max='4' />
-            <CreateCheckbox className="aa" field="program" name={'BE'} value={'BE'} checkBoxes={this.updateProgram} />
-            <CreateCheckbox field="program" name={'FE'} value={'FE'} checkBoxes={this.updateProgram} />
+            <CreateInput field="cohort" text="Cohort (ex: 1406)" value={cohort !== 0 ? cohort : ''} handleChange={this.updateCohort} max='4' />
+            <CreateCheckbox className="aa" field="program" name={'BE'} value={'BE'} checkBoxes={this.updateProgram} checked={program === 'BE' ? true : false} />
+            <CreateCheckbox field="program" name={'FE'} value={'FE'} checkBoxes={this.updateProgram} checked={program === 'FE' ? true : false} />
           </div>
         </form>
         <div className="input-box">
