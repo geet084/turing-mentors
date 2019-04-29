83dc81be5f70c0734d8f7c3e5c5d607cdd0c267f
Travis Gee <geet084@gmail.com>
2019-04-29 07:37:47 -0600

    Adjusts Contribute component to be state-less

--- src/components/Contribute/Contribute.js
+++ src/components/Contribute/Contribute.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export class Contribute extends Component {
-  render() {
-    return (
-      <div className="link-content pages">
-        <h2>Want to contribute?</h2>
-        <p>Please send questions to:</p>
-        <h4 className="dev-links">BE: <a href="#">Ricardo Ledesma</a></h4>
-        <h4 className="dev-links">FE: <a href="#">Travis Gee</a></h4>
-      </div>
-    )
-  }
+export const Contribute = () => {
+  return (
+    <div className="link-content">
+      <h2>Want to contribute?</h2>
+      <p>Please send questions to:</p>
+      <h4 className="dev-links">BE: <a href="#">Ricardo Ledesma</a></h4>
+      <h4 className="dev-links">FE: <a href="#">Travis Gee</a></h4>
+    </div>
+  )
 }
 
 export default Contribute;
