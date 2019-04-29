a1d812508154604d10c5eea39c009c430a009573
Travis Gee <geet084@gmail.com>
2019-04-29 07:31:48 -0600

    Adjusts About component to be state-less

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export class About extends Component {
-  render() {
-    return (
-      <div className="link-content pages">
-        <p>
-          Turing's mission is to unlock human potential by training a diverse and inclusive student body to succeed in high-fulfillment technical careers. We are a community committed to challenging ourselves and each other to build the knowledge, skills, and understanding needed for long-term success.
-          </p>
-        <p>
-          In addition to that committment is the willingness to help others who are walking in your footsteps, struggling in the same ways you may have struggled.
-          </p>
-        <h5>
-          Turing Mentors offers a way to connect those on both sides of the Turing program to the betterment of all.
-          </h5>
-      </div>
-    )
-  }
+export const About = () => {
+  return (
+    <div className="link-content">
+      <p>
+        Turing's mission is to unlock human potential by training a diverse and inclusive student body to succeed in high-fulfillment technical careers. We are a community committed to challenging ourselves and each other to build the knowledge, skills, and understanding needed for long-term success.
+        </p>
+      <p>
+        In addition to that committment is the willingness to help others who are walking in your footsteps, struggling in the same ways you may have struggled.
+        </p>
+      <h5>
+        Turing Mentors offers a way to connect those on both sides of the Turing program to the betterment of all.
+        </h5>
+    </div>
+  )
 }
 
 export default About;
