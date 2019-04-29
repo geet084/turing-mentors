ef79ed7d3b4e370b85eb0d8b53eb1ec9c6e4de43
Travis Gee <geet084@gmail.com>
2019-04-29 07:47:06 -0600

    Removes Contribute component style sheet

--- src/components/Contribute/Contribute.js
+++ src/components/Contribute/Contribute.js
@@ -5,8 +5,8 @@ export const Contribute = () => {
     <div className="link-content">
       <h2>Want to contribute?</h2>
       <p>Please send questions to:</p>
-      <h4 className="dev-links">BE: <a href="#">Ricardo Ledesma</a></h4>
-      <h4 className="dev-links">FE: <a href="#">Travis Gee</a></h4>
+      <h4 className="link-style">BE: <a href="https://github.com/stoic-plus">Ricardo Ledesma</a></h4>
+      <h4 className="link-style">FE: <a href="https://github.com/geet084">Travis Gee</a></h4>
     </div>
   )
 }
